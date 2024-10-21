094d367a1c798f116223b593e9bb79fdc8f21d8a
Ana Heloisa <heloisaangelo.17@gmail.com>
2024-10-21 16:46:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let deck = pormpt('A quantidade de carta é: 52 cartas.\n Opções:\n 1. Adicionar uma carta.\n 2. Puxar uma carta.\n 3. Sair. ')
+ switch(deck){
+case 1:
+    prompt('Qual carta deseja adicionar?')
+ }
